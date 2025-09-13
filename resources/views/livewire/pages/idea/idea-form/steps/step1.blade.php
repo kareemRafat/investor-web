72dee1065ddb95acee8e952fd2e39cf47aca53c3
kareemRafat <kareem.force@gmail.com>
2025-09-13 20:38:35 +0300

    working on fixing the validation messages

--- resources/views/livewire/pages/idea/idea-form/steps/step1.blade.php
+++ resources/views/livewire/pages/idea/idea-form/steps/step1.blade.php
@@ -3,16 +3,12 @@
     <x-pages.idea-wizard.idea-header title="{{ __('pages/mainpage.submit_idea') }}"
         subtitle="{{ __('idea.steps.step1.subtitle') }}" />
 
-    @error('ideaField')
-        <div class="text-danger small mt-2">{{ $message }}</div>
-    @enderror
-
     <div class="step_height bg-white rounded-8 shadow-sm p-3 p-md-3 p-lg-4">
         <div class="row g-3 justify-content-center">
             @foreach ($ideaOptions as $key => $label)
                 <div class="col-6 col-lg-3 col-md-6">
                     <input type="radio" class="btn-check" wire:model="ideaField" id="idea-{{ $key }}"
-                        value="{{ $key }}" autocomplete="off" name="ideaField" >
+                        value="{{ $key }}" autocomplete="off" name="ideaField">
 
                     <label
                         class="btn btn-outline-primary w-100 h-100 px-1 px-md-2 py-3 rounded-8 shadow-sm fw-bold small"
@@ -23,4 +19,7 @@
             @endforeach
         </div>
     </div>
+    @error('ideaField')
+        <div class="text-danger text-center fw-bold mt-2">{{ $message }}</div>
+    @enderror
 </div>
