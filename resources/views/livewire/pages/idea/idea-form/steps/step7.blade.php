7b92edf4ec3019febdf4a22f765ee7cecb1b79bb
= <kareem.force@gmail.com>
2025-10-07 02:31:34 +0300

    fix step 7

--- resources/views/livewire/pages/idea/idea-form/steps/step7.blade.php
+++ resources/views/livewire/pages/idea/idea-form/steps/step7.blade.php
@@ -29,38 +29,39 @@
                 <div class="row g-3">
 
                     <!-- بيع الفكرة -->
-                    <div class="col-lg-6 col-12 my-2">
-                        <div class="d-flex align-items-center justify-content-between">
-                            <div class="col-12 col-md-12">
-                                <input type="radio" class="btn-check" id="needs"
-                                    wire:model="data.contribute_type"
-                                    x-model="contribute_type"
-                                    @change="resetFields('sell')"
-                                    value="sell" name="contribute_type" autocomplete="off">
-                                <label
-                                    class="btn btn-outline-primary w-100 h-100 px-1 px-md-2 py-3 rounded-8 shadow-sm fw-bold small"
-                                    for="needs">
-                                    {{ __('idea.steps.step7.sell') }}
-                                </label>
+                    <div class="col-12">
+                        <div class="col-lg-7 col-12 my-2 ps-2">
+                            <div class="d-flex align-items-center justify-content-between">
+                                <div class="col-12 col-md-12">
+                                    <input type="radio" class="btn-check" id="needs"
+                                        wire:model="data.contribute_type" x-model="contribute_type"
+                                        @change="resetFields('sell')" value="sell" name="contribute_type"
+                                        autocomplete="off">
+                                    <label
+                                        class="btn btn-outline-primary w-100 h-100 px-1 px-md-2 py-3 rounded-8 shadow-sm fw-bold small"
+                                        for="needs">
+                                        {{ __('idea.steps.step7.sell') }}
+                                    </label>
+                                </div>
                             </div>
                         </div>
                     </div>
 
                     <!-- المساهمة بالفكرة فقط -->
-                    <div class="col-lg-6 col-12 my-2">
-                        <div class="d-flex align-items-center justify-content-between">
-                            <div class="col-12 col-md-12">
-                                <input type="radio" class="btn-check" id="contribute"
-                                    wire:model="data.contribute_type"
-                                    x-model="contribute_type"
-                                    @change="resetFields('idea')"
-                                    value="idea" name="contribute_type"
-                                    autocomplete="off">
-                                <label
-                                    class="btn btn-outline-primary w-100 h-100 px-1 px-md-2 py-3 rounded-8 shadow-sm fw-bold small"
-                                    for="contribute">
-                                    {{ __('idea.steps.step7.idea') }}
-                                </label>
+                    <div class="col-12">
+                        <div class="col-lg-7 col-12 mb-2 ps-2">
+                            <div class="d-flex align-items-center justify-content-between">
+                                <div class="col-12 col-md-12">
+                                    <input type="radio" class="btn-check" id="contribute"
+                                        wire:model="data.contribute_type" x-model="contribute_type"
+                                        @change="resetFields('idea')" value="idea" name="contribute_type"
+                                        autocomplete="off">
+                                    <label
+                                        class="btn btn-outline-primary w-100 h-100 px-1 px-md-2 py-3 rounded-8 shadow-sm fw-bold small"
+                                        for="contribute">
+                                        {{ __('idea.steps.step7.idea') }}
+                                    </label>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -71,10 +72,8 @@
                             <div class="d-flex align-items-center justify-content-between">
                                 <div class="col-12 col-md-12">
                                     <input type="radio" class="btn-check" id="contribute_person"
-                                        wire:model="data.contribute_type"
-                                        x-model="contribute_type"
-                                        @change="resetFields('capital')"
-                                        value="capital" name="contribute_type"
+                                        wire:model="data.contribute_type" x-model="contribute_type"
+                                        @change="resetFields('capital')" value="capital" name="contribute_type"
                                         autocomplete="off">
                                     <label
                                         class="btn btn-outline-primary w-100 h-100 px-1 px-md-2 py-3 rounded-8 shadow-sm fw-bold small"
@@ -86,31 +85,24 @@
                         </div>
 
                         <div class="col-lg-5 col-12 mt-3 mt-lg-0 mb-2 mb-lg-0">
-                            <div
-                                class="d-flex align-items-center gap-2 flex-wrap flex-lg-nowrap gap-md-3 gap-lg-4">
+                            <div class="d-flex align-items-center gap-2 flex-wrap flex-lg-nowrap gap-md-3 gap-lg-4">
                                 <div class="d-flex align-items-center gap-2">
                                     <input type="radio" class="form-check-input" id="staff_yes"
-                                        wire:model="data.staff"
-                                        x-model="staff"
-                                        value="full_time" name="staff">
+                                        wire:model="data.staff" x-model="staff" value="full_time" name="staff">
                                     <label class="form-check-label" for="staff_yes">
                                         {{ __('idea.steps.step7.full_time') }}
                                     </label>
                                 </div>
                                 <div class="d-flex align-items-center gap-2">
                                     <input type="radio" class="form-check-input" id="staff_no"
-                                        wire:model="data.staff"
-                                        x-model="staff"
-                                        value="part_time" name="staff">
+                                        wire:model="data.staff" x-model="staff" value="part_time" name="staff">
                                     <label class="form-check-label" for="staff_no">
                                         {{ __('idea.steps.step7.part_time') }}
                                     </label>
                                 </div>
                                 <div class="d-flex align-items-center gap-2">
                                     <input type="radio" class="form-check-input" id="admin_only_no"
-                                        wire:model="data.staff"
-                                        x-model="staff"
-                                        value="supervision" name="staff">
+                                        wire:model="data.staff" x-model="staff" value="supervision" name="staff">
                                     <label class="form-check-label" for="admin_only_no">
                                         {{ __('idea.steps.step7.supervision') }}
                                     </label>
@@ -125,10 +117,8 @@
                             <div class="d-flex align-items-center justify-content-between">
                                 <div class="col-12 col-md-12">
                                     <input type="radio" class="btn-check" id="contribute_money"
-                                        wire:model="data.contribute_type"
-                                        x-model="contribute_type"
-                                        @change="resetFields('personal')"
-                                        value="personal" name="contribute_type"
+                                        wire:model="data.contribute_type" x-model="contribute_type"
+                                        @change="resetFields('personal')" value="personal" name="contribute_type"
                                         autocomplete="off">
                                     <label
                                         class="btn btn-outline-primary w-100 h-100 px-1 px-md-2 py-3 rounded-8 shadow-sm fw-bold small"
@@ -145,18 +135,16 @@
                                         {{ __('idea.steps.step7.amount') }}
                                     </label>
                                     <input type="number" class="form-control py-3 rounded-8" id="money_amount"
-                                        wire:model="data.money_amount"
-                                        x-model="money_amount"
-                                        name="money_amount" placeholder="" />
+                                        wire:model="data.money_amount" x-model="money_amount" name="money_amount"
+                                        placeholder="$" />
                                 </div>
                                 <div class="d-flex align-items-center gap-3">
                                     <label for="money_percent" class="text-primary fw-bold fs-5">
                                         {{ __('idea.steps.step7.percent') }}
                                     </label>
                                     <input type="number" class="form-control py-3 rounded-8" id="money_percent"
-                                        wire:model="data.money_percent"
-                                        x-model="money_percent"
-                                        name="money_percent" placeholder="" />
+                                        wire:model="data.money_percent" x-model="money_percent" name="money_percent"
+                                        placeholder="" />
                                 </div>
                             </div>
                         </div>
@@ -168,10 +156,8 @@
                             <div class="d-flex align-items-center justify-content-between">
                                 <div class="col-12 col-md-12">
                                     <input type="radio" class="btn-check" id="contribute_person_and_money"
-                                        wire:model="data.contribute_type"
-                                        x-model="contribute_type"
-                                        @change="resetFields('both')"
-                                        value="both" name="contribute_type"
+                                        wire:model="data.contribute_type" x-model="contribute_type"
+                                        @change="resetFields('both')" value="both" name="contribute_type"
                                         autocomplete="off">
                                     <label
                                         class="btn btn-outline-primary w-100 h-100 px-1 px-md-2 py-3 rounded-8 shadow-sm fw-bold small"
@@ -191,8 +177,8 @@
                                         </label>
                                         <input type="number" class="form-control py-3 rounded-8"
                                             id="person_money_amount" wire:model="data.person_money_amount"
-                                            x-model="person_money_amount"
-                                            name="person_money_amount" placeholder="$" />
+                                            x-model="person_money_amount" name="person_money_amount"
+                                            placeholder="$" />
                                     </div>
                                     <div class="d-flex align-items-center gap-2">
                                         <label for="person_money_percent" class="text-primary fw-bold fs-5">
@@ -200,27 +186,23 @@
                                         </label>
                                         <input type="number" class="form-control py-3 rounded-8"
                                             id="person_money_percent" wire:model="data.person_money_percent"
-                                            x-model="person_money_percent"
-                                            name="person_money_percent" placeholder="" />
+                                            x-model="person_money_percent" name="person_money_percent"
+                                            placeholder="" />
                                     </div>
                                 </div>
                                 <div class="d-flex align-items-center gap-2 flex-wrap">
                                     <div class="d-flex align-items-center gap-2">
                                         <input type="radio" class="form-check-input" id="staff_yes_person_money"
-                                            wire:model="data.staff_person_money"
-                                            x-model="staff_person_money"
-                                            value="full_time"
-                                            name="staff_person_money">
+                                            wire:model="data.staff_person_money" x-model="staff_person_money"
+                                            value="full_time" name="staff_person_money">
                                         <label class="form-check-label" for="staff_yes_person_money">
                                             {{ __('idea.steps.step7.full_time') }}
                                         </label>
                                     </div>
                                     <div class="d-flex align-items-center gap-2">
                                         <input type="radio" class="form-check-input" id="staff_no_person_money"
-                                            wire:model="data.staff_person_money"
-                                            x-model="staff_person_money"
-                                            value="part_time"
-                                            name="staff_person_money">
+                                            wire:model="data.staff_person_money" x-model="staff_person_money"
+                                            value="part_time" name="staff_person_money">
                                         <label class="form-check-label" for="staff_no_person_money">
                                             {{ __('idea.steps.step7.part_time') }}
                                         </label>
@@ -228,8 +210,8 @@
                                     <div class="d-flex align-items-center gap-2">
                                         <input type="radio" class="form-check-input"
                                             id="admin_only_no_person_money" wire:model="data.staff_person_money"
-                                            x-model="staff_person_money"
-                                            value="supervision" name="staff_person_money">
+                                            x-model="staff_person_money" value="supervision"
+                                            name="staff_person_money">
                                         <label class="form-check-label" for="admin_only_no_person_money">
                                             {{ __('idea.steps.step7.supervision') }}
                                         </label>
@@ -238,18 +220,16 @@
                             </div>
                         </div>
                     </div>
-
-                    {{-- Errors --}}
-                    <div class="col-12 mt-3">
-                        @if ($errors->any())
-                            <span class="text-white bg-danger rounded py-2 px-4 text-center fw-bold">
-                                {{ $errors->first() }}
-                            </span>
-                        @endif
-                    </div>
-
                 </div>
             </div>
         </div>
+    </div>
+    {{-- Errors --}}
+    <div class="d-flex flex-column align-items-center mt-2">
+        @if ($errors->any())
+            <span class="text-white bg-danger rounded py-2 px-4 text-center fw-bold">
+                {{ $errors->first() }}
+            </span>
+        @endif
     </div>
 </div>
