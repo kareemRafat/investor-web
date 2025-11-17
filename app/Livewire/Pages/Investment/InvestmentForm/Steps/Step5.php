793417fcccc2e706b2d4ce78416c896da761fdcb
= <kareem.force@gmail.com>
2025-11-18 01:43:11 +0200

    adding step5 investor new wizard step (working on not finished)

--- app/Livewire/Pages/Investment/InvestmentForm/Steps/Step5.php
+++ app/Livewire/Pages/Investment/InvestmentForm/Steps/Step5.php
@@ -3,11 +3,22 @@
 namespace App\Livewire\Pages\Investment\InvestmentForm\Steps;
 
 use Livewire\Component;
+use Livewire\Attributes\On;
 
 class Step5 extends Component
 {
     public function render()
     {
         return view('livewire.pages.investment.investment-form.steps.step5');
+    }
+
+    #[On('validate-step-5')]
+    public function validateStep5()
+    {
+        // $this->validate();
+
+        // $this->syncContribution();
+
+        $this->dispatch('go-to-next-step');
     }
 }
