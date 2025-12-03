023afc8f0bf06d77d78ea27f82fbcdeb9d2df1d1
kareemRafat <kareem.force@gmail.com>
2025-12-03 20:37:40 +0200

    remove unessary code

--- resources/views/livewire/pages/idea/components/idea-filters.blade.php
+++ resources/views/livewire/pages/idea/components/idea-filters.blade.php
@@ -5,7 +5,7 @@
                 <select class="form-select py-2" wire:model.live="field">
                      {{-- get Fields from translation files (no query Needed) --}}
                     <option selected disabled value="">{{ __('idea.index.filter_field') }}</option>
-                    @foreach (__('idea.steps.step1.options', [], app()->getLocale()) as $key => $value)
+                    @foreach (__('idea.steps.step1.options') as $key => $value)
                         <option value="{{ $key }}">{{ $value }}</option>
                     @endforeach
                 </select>
@@ -14,7 +14,7 @@
                 <select class="form-select py-2" wire:model.live="country">
                     {{-- get Countries from translation files (no query Needed) --}}
                     <option selected disabled value="">{{ __('idea.index.filter_countries') }}</option>
-                    @foreach (__('idea.steps.step2.options', [], app()->getLocale()) as $country)
+                    @foreach (__('idea.steps.step2.options') as $country)
                         <option value="{{ $country['code'] }}">{{ $country['name'] }}</option>
                     @endforeach
                 </select>
